290a4d3811bce8dc2207cad9cd6da760637e536a
ZhuMon <richardlin0212@gmail.com>
2022-02-27 00:59:58 +0800

    Add standard main

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdarg.h>
+
+
+int main()
+{
+    return 0;
+}
--- my_mkdir.sh
+++ my_mkdir.sh
@@ -1,3 +1,4 @@
 mkdir -p $1
 cp -f Makefile $1/Makefile || exit 1
+cp -f main.c $1/main.c || exit 1
 ln -sf .clang-format $1/ || exit 1
